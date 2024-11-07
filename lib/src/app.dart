19d7cfb2733ef9f971b0ab735f177630a05c4f63
alexisflo <forthingsLL@hotmail.com>
2024-11-07 17:35:21 -0600

    Posible alert dialog

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+/*import 'package:flutter/material.dart';
+
+// const String _customText = "Este es un mensaje de que has enviado una alerta";
+
+const List<String> _textCustom = [
+  "Mensaje de demostración",
+  "Mensaje de alerta",
+  "Haciendo llamada",
+  "Reportando"
+];
+
+class AppDialog {
+  final int selectedText;
+
+  AppDialog({this.selectedText = 0})
+      : assert(selectedText >= 0, 'la opción debe de estar entre 0 y 3');
+
+  AlertDialog aler()
+}
+*/
