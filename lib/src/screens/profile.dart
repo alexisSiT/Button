dc151c63ad29c970ae26cc31f69da4bf7b33c12b
alexisflo <forthingsLL@hotmail.com>
2024-11-07 17:35:39 -0600

    profile screen

--- lib/src/screens/profile.dart
+++ lib/src/screens/profile.dart
@@ -1,3 +1,9 @@
-void main() {
-  
+  import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  static const title = 'Profile';
+  @override
+  Widget build(BuildContext context) {
+    
+  }
 }
